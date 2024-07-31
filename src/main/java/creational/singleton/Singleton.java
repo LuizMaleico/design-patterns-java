4993a57515f2baeab2acd33bb903a9e4f1e0c889
LuizMaleico <arthurmaleico@gmail.com>
2024-07-31 20:59:41 -0300

    Singleton

---
+++ src/main/java/creational/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package creational.singleton;
+
+public class Singleton {
+    private static Singleton instancia;
+
+    private Singleton() {}
+
+    public static Singleton getInstance(){
+        if(instancia == null){
+            instancia = new Singleton();
+        }
+        return instancia;
+    }
+
+}
